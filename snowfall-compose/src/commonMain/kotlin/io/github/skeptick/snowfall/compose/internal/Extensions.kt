c3ac89b7afb46fb8fe145d24cd77c620d63adefd
lavafrai <lavafrai@yandex.ru>
2024-12-29 22:41:44 +0300

    fix: AlphaOffsetRange isolated as a separate constant

--- snowfall-compose/src/commonMain/kotlin/io/github/skeptick/snowfall/compose/internal/Extensions.kt
+++ snowfall-compose/src/commonMain/kotlin/io/github/skeptick/snowfall/compose/internal/Extensions.kt
@@ -3,6 +3,7 @@ package io.github.skeptick.snowfall.compose.internal
 import androidx.compose.ui.graphics.Path
 
 internal val SourceAngleRange = 1.45f..1.55f
+internal val AlphaOffsetRange = 0.5f..1f
 internal val AddableAngleRange = -0.0025f..0.0025f
 
 internal operator fun Float.times(range: ClosedRange<Float>) =
